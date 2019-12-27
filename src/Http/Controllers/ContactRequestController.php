5f874b76dace2f4aeb35256767c11c2e37257da5
Mohammed <mohammed@heber-net.net>
2019-12-27 23:01:40 +0100

    setup controller

--- src/Http/Controllers/ContactRequestController.php
+++ src/Http/Controllers/ContactRequestController.php
@@ -7,6 +7,9 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Laracontact\Events\ContactRequestEvent;
+use Laracontact\Mail\ContactRequestMail;
 use Laracontact\Models\ContactRequest;
 
 class ContactRequestController extends BaseController
@@ -18,18 +21,36 @@ class ContactRequestController extends BaseController
      *
      * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
-     * @throws \Illuminate\Validation\ValidationException
      */
     public function store(Request $request)
     {
-       $valid_data =  $this->validate($request, [
+       $valid_data =  $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|email',
             'subject' => 'required|string|max:255',
             'message' => 'required|string',
         ]);
-        ContactRequest::create($valid_data);
+
+       $contact_request =  ContactRequest::create($valid_data);
+
+       if(config('contact_request.send_mails'))
+        $this->sendMails($contact_request);
+
+       event(new ContactRequestEvent($contact_request));
 
         return redirect(config('contact_request.redirectTo', '/'));
+    }
+
+    /**
+     * send the contact request to notifiables
+     * @param array $valid_data
+     */
+    protected function sendMails(ContactRequest $contact_request): void
+    {
+        $notifiables = collect(config('contact_request.notifiables'));
+
+        $notifiables->each(function ($email) use ($contact_request) {
+            Mail::to($email)->send(new ContactRequestMail($contact_request));
+        });
     }
 }
