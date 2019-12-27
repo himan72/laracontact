04ab92aaf0c067aa472f1904938d22c6552b2421
Mohammed <mohammed@heber-net.net>
2019-12-27 15:32:16 +0100

    register routes and views

--- src/LaracontactServiceProvider.php
+++ src/LaracontactServiceProvider.php
@@ -11,6 +11,9 @@ class LaracontactServiceProvider extends ServiceProvider
     public function boot()
     {
 
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'laracontact');
+
     }
 
     public function register()
