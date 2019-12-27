7aa60ebce93568156f2857f231086902f0aefdda
Mohammed <mohammed@heber-net.net>
2019-12-27 19:22:46 +0100

    refactoring routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,4 @@
 <?php
-Route::group(['namespace' => 'Laracontact\Http\Controllers'], function() {
-    Route::get('contact-us', 'ContactUsController@show')->name('contact-us.show');
-    Route::post('contact-us', 'ContactUsController@store')->name('contact-us.store');
-});
 
 Route::view('contact-us', 'laracontact::contact');
+Route::post('contact-us', 'Laracontact\Http\Controllers\ContactRequestController@store')->name('contact_us.store');
