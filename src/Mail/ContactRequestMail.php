f05d61283265e8cfb5a2b3c099ff76d11151373c
Mohammed <mohammed@heber-net.net>
2019-12-27 23:02:04 +0100

    add maillable

---
+++ src/Mail/ContactRequestMail.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Laracontact\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Laracontact\Models\ContactRequest;
+
+class ContactRequestMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * @var \Laracontact\Models\ContactRequest
+     */
+    public $contact_request;
+
+    /**
+     * ContactRequestMail constructor.
+     *
+     * @param \Laracontact\Models\ContactRequest $contact_request
+     */
+    public function __construct(ContactRequest $contact_request)
+    {
+        $this->contact_request = $contact_request;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->markdown('laracontact::contact_request.emails.admin');
+    }
+}
