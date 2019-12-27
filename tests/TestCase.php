f4cdd063f0f8237f1a3ad312bc8c7090656b3b09
Mohammed <mohammed@heber-net.net>
2019-12-27 19:22:56 +0100

    add setup

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,6 +5,26 @@ namespace Laracontact\Tests;
 class TestCase extends   \Orchestra\Testbench\TestCase
 {
 
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     *
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('database.default', 'laracontacttest');
+        $app['config']->set('database.connections.laracontacttest', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
+
+        $app['config']->set('contact_request.redirectTo', '/thanks-message');
+    }
+
     protected function getPackageProviders($app)
     {
         return ['Laracontact\LaracontactServiceProvider'];
