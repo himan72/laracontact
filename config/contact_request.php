7a63b079c9353364346c670b4df481aac592cc19
Mohammed <mohammed@heber-net.net>
2019-12-27 23:00:02 +0100

    add comments

--- config/contact_request.php
+++ config/contact_request.php
@@ -1,5 +1,18 @@
 <?php
 return [
-    'redirectTo' => '/'
+    /*
+     * Set the url redirection after submitting the contact us form.
+     */
+    'redirectTo' => '/',
+
+    /*
+     * Array of emails that should receive the contact request
+     */
+    'notifiables' => ['admin@test.test'],
+
+    /*
+     * Set to false to disable sending emails feature
+     */
+    'send_mails' => true,
 
 ];
