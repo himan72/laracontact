e090410a3c276a6b3a5ebc6bc1a87b294402fa44
Mohammed <mohammed@heber-net.net>
2019-12-27 23:01:25 +0100

    rename route

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,4 @@
 <?php
 
-Route::view('contact-us', 'laracontact::contact');
+Route::view('contact-us', 'laracontact::contact_request.contact');
 Route::post('contact-us', 'Laracontact\Http\Controllers\ContactRequestController@store')->name('contact_us.store');
