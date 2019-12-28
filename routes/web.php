bab7c3b6558a4e500cf1bd0cb3d301156eb7489e
Mohammed <mohammed@heber-net.net>
2019-12-28 11:17:37 +0100

    refactor path from config

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
 Route::group(['middleware' => ['web']], function () {
-    Route::view('contact-us', 'laracontact::contact_request.contact');
-    Route::post('contact-us', 'Laracontact\Http\Controllers\ContactRequestController@store')->name('contact_us.store');
+    Route::view(config('contact_request.form_path', 'contact-us'), 'laracontact::contact_request.contact');
+    Route::post(config('contact_request.form_path', 'contact-us'), 'Laracontact\Http\Controllers\ContactRequestController@store')->name('contact_us.store');
 });
