14c67b145a7b55af25d018b3bf3e03416504a079
Mohammed <mohammed@heber-net.net>
2019-12-28 09:54:58 +0100

    add recpatcha v2 validator

---
+++ src/Validators/Recaptcha.php
@@ -0,0 +1,21 @@
+<?php
+namespace Laracontact\Validators;
+use GuzzleHttp\Client;
+class ReCaptcha
+{
+    public function validate($attribute, $value, $parameters, $validator)
+    {
+        $client = new Client;
+        $response = $client->post('https://www.google.com/recaptcha/api/siteverify',
+            [
+                'form_params' =>
+                    [
+                        'secret' => env('RECAPTCHA_SECRET'),
+                        'response' => $value
+                    ]
+            ]
+        );
+        $body = json_decode((string)$response->getBody());
+        return $body->success;
+    }
+}
