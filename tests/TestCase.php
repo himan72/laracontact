77a3627c11ee0835ba555f2546682ff2c2079ba5
Mohammed <mohammed@heber-net.net>
2019-12-28 09:55:06 +0100

    add tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -23,6 +23,7 @@ class TestCase extends   \Orchestra\Testbench\TestCase
      */
     protected function getEnvironmentSetUp($app)
     {
+
         $app['config']->set('database.default', 'laracontacttest');
         $app['config']->set('database.connections.laracontacttest', [
             'driver'   => 'sqlite',
@@ -33,6 +34,12 @@ class TestCase extends   \Orchestra\Testbench\TestCase
         $app['config']->set('contact_request.redirectTo', '/thanks-message');
         $app['config']->set('contact_request.notifiables', ['test1@test.com', 'test2@test.com', 'test3@test.com']);
         $app['config']->set('contact_request.send_mails', true);
+        $app['config']->set('contact_request.recapcha.recaptcha_secret', env('RECAPTCHA_SECRET'));
+        $app['config']->set('contact_request.recapcha.recaptcha_recaptcha_sitekey', env('RECAPTCHA_SITEKEY'));
+
+        dd(config('contact_request.recapcha.recaptcha_secret'));
+
+
     }
 
     protected function getPackageProviders($app)
