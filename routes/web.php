1dc6be1bcdb1e7918999fd71d3b40db4a360d417
Mohammed <mohammed@heber-net.net>
2019-12-28 09:54:22 +0100

    add web middleware to routes

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
-
-Route::view('contact-us', 'laracontact::contact_request.contact');
-Route::post('contact-us', 'Laracontact\Http\Controllers\ContactRequestController@store')->name('contact_us.store');
+Route::group(['middleware' => ['web']], function () {
+    Route::view('contact-us', 'laracontact::contact_request.contact');
+    Route::post('contact-us', 'Laracontact\Http\Controllers\ContactRequestController@store')->name('contact_us.store');
+});
